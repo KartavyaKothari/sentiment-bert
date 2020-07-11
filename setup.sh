fcdeb3940de4e69b7dbc6c03ef1e6e6825c55b0c
Kartavya Kothari <kartavya.kothari24@gmail.com>
2020-07-11 22:47:39 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ cd data
 wget www.cse.iitb.ac.in/~kartavya/download
 unzip download
 rm download
+cd ..
 conda install pytorch==1.2.0 torchvision==0.4.0 cudatoolkit=9.2 -c pytorch
 pip install -r requirements.txt
